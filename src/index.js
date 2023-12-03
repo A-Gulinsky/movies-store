0fb6386847e04a23db656fa83acf42c7fc423cf2
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-03 19:35:06 +0200

    15 commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,34 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
+import { hydrate, render } from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <HelmetProvider>
+//         <App />
+//       </HelmetProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+const APP = (<React.StrictMode>
     <BrowserRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
     </BrowserRouter>
-  </React.StrictMode>
-);
+</React.StrictMode>
+)
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
